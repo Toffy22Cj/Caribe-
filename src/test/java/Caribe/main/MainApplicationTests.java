095e0912466fc8b071e2536faa8ada3c3b0ece35
Carlos <cjpp12220@gmail.com>
2026-06-21 18:48:15 -0500

    primero

---
+++ src/test/java/Caribe/main/MainApplicationTests.java
@@ -0,0 +1,15 @@
+package Caribe.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
